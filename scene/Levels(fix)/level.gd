52dd904649c7fc41ca5e4c88afa4176fa5804855
alkmanistik <erik.fattakhov.04@mail.ru>
2023-10-06 15:30:29 +0500

    hidebox

--- scene/Levels(fix)/level.gd
+++ scene/Levels(fix)/level.gd
@@ -1,2 +1,12 @@
 extends Node3D
 
+@onready var player = $Player
+@onready var spawn_position: Marker3D = $SpawnPosition
+@onready var ui: CanvasLayer = $UI
+
+func _ready():
+	player.position = spawn_position.position
+
+
+func _on_respawn_zone_body_entered(body):
+	player.position = spawn_position.position
