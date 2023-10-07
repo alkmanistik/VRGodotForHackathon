9758a6fe442f6c47043b8c43fdc9d446df8eb8fd
alkmanistik <erik.fattakhov.04@mail.ru>
2023-10-07 18:44:43 +0500

    add save sistem

--- scene/Objects/Dynamic/Treasure.gd
+++ scene/Objects/Dynamic/Treasure.gd
@@ -21,7 +21,6 @@ func open():
 func _on_area_3d_body_entered(_body):
 	if !is_open and can_open:
 		open()
-		$"../ChestOpen".play()
 		is_open = true
 		
 
--- scene/Objects/Dynamic/key.gd
+++ scene/Objects/Dynamic/key.gd
@@ -2,6 +2,10 @@ extends Node3D
 
 signal add_key()
 
+func _process(delta):
+	look_at(get_tree().get_first_node_in_group("player").global_position)
+	rotation.x = 0
+
 func _on_area_3d_body_entered(_body):
 	add_key.emit()
 	queue_free()
