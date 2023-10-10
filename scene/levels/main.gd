0b18659c3e7ac8805af8841f2d39914265629556
gizdun <danil-2004@list.ru>
2023-10-10 19:23:03 +0500

    cube

--- scene/levels/main.gd
+++ scene/levels/main.gd
@@ -1,6 +1,8 @@
 extends Node3D
 
-
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("nkey"):
+		get_node("cube/Destruction").destroy()
 
 func _on_respawn_zone_body_exited(body: Node3D) -> void:
 	body.position = $SpawnPosition.position
