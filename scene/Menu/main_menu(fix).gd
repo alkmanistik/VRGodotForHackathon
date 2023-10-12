f6e9f1385a79a80e4f8430af1b6478dd09a9117e
alkmanistik <erik.fattakhov.04@mail.ru>
2023-10-12 10:16:12 +0500

    Revert "fix level menu"
    This reverts commit 50a033f75d9d1ac2e50cb22e54bb4a2498579871.

--- scene/Menu/main_menu(fix).gd
+++ scene/Menu/main_menu(fix).gd
@@ -1,8 +1,8 @@
 extends CanvasLayer
 
 @onready var viewport = get_tree().get_first_node_in_group("viewport_menu")
-var levels_menu = preload("res://scene/Menu/level_menu(fix).tscn")
-var settings_menu = preload("res://scene/Menu/settengs_menu(fix).tscn")
+@onready var levels_menu = preload("res://scene/Menu/levels_menu.tscn")
+@onready var settings_menu = preload("res://scene/Menu/settengs_menu(fix).tscn")
 
 
 func _on_start_button_pressed():
